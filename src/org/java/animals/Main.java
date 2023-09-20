3cc6b0d1e88f0f90b5bac9333b7bbe9e0bc7cfc7
Pietromuresu <muresupietro66@gmail.com>
2023-09-20 15:01:17 +0200

    Classi Astratte - end

--- src/org/java/animals/Main.java
+++ src/org/java/animals/Main.java
@@ -4,12 +4,30 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Animal[] animali = new Animal[5];
-		
+		System.out.println("\n ------------Cane-------------- ");
 		Cane cane1 = new Cane(2, "Bobbi");
 		System.out.println(cane1);
 		cane1.dormi();
 		cane1.verso();
 		cane1.mangia();
+		System.out.println("\n -----------Passerotto------------- ");
+		Passerotto passerotto1 = new Passerotto(2, "Cip Ciop");
+		System.out.println(passerotto1);
+		passerotto1.dormi();
+		passerotto1.verso();
+		passerotto1.mangia();
+		System.out.println("\n ------------Aquila------------- ");
+		Aquila aquila1 = new Aquila(2, "Eagle");
+		System.out.println(aquila1);
+		aquila1.dormi();
+		aquila1.verso();
+		aquila1.mangia();
+		System.out.println("\n -----------Delfino--------------- ");
+		Delfino delfino1 = new Delfino(0, "SQL");
+		System.out.println(delfino1);
+		delfino1.dormi();
+		delfino1.verso();
+		delfino1.mangia();
+		System.out.println("\n --------------------------------- ");
 	}
 }
